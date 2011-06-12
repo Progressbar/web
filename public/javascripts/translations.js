e599f4a3379d2550eaf8165c934fcfc1055bb58b
progressbarsk <marek@keram.name>
2011-06-13 01:44:04 +0200

    init commit

---
+++ public/javascripts/translations.js
@@ -0,0 +1,2 @@
+var I18n = I18n || {};
+I18n.translations = {"fi":{"js":{"admin":{"confirm_changes":"Kaikki muutoksesi menetet\u00e4\u00e4n. Oletko varma, ett\u00e4 haluat jatkaa tallentamatta?"}}},"en":{"js":{"admin":{"confirm_changes":"Any changes you've made will be lost. Are you sure you want to continue without saving?"}}}};
