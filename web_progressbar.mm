47503ca82dd5194312924934a232f4320e7daadb
= <marek@keram.name>
2011-06-25 15:37:17 +0200

    freemind structure off website

---
+++ web_progressbar.mm
@@ -0,0 +1,71 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1309005209887" ID="ID_1875552406" MODIFIED="1309005218848" TEXT="web progressbar">
+<node CREATED="1309005221560" ID="ID_1276704012" MODIFIED="1309006222318" POSITION="right" TEXT="uvod">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309005223633" ID="ID_1616425992" MODIFIED="1309006226115" POSITION="right" TEXT="kalendar">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309005228628" ID="ID_573244402" MODIFIED="1309006229199" POSITION="right" TEXT="blog">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309005230347" ID="ID_1450522333" MODIFIED="1309006232268" POSITION="right" TEXT="projekty">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1309005298661" ID="ID_218882334" MODIFIED="1309006305485" TEXT="zh&#xe1;name">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1309005689156" ID="ID_495269297" MODIFIED="1309006006796" TEXT="Priebezne aktualizovany zoznam, naradia, hardwaru, materialu pre potreby jednotlivych projektov. zaroven by nemusel ist len o zoznam veci pre progressbar ale i inych hackerspacov na okoli."/>
+</node>
+<node CREATED="1309005338193" ID="ID_110776114" MODIFIED="1309006443725" TEXT="Zakladne informacie o jednotlivych projektoch (co, kto, preco)  s prelinkovanim na stranku projektu na wiki."/>
+</node>
+<node CREATED="1309005232546" ID="ID_1339823018" MODIFIED="1309006280172" POSITION="right" TEXT="oficiality">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1309005354548" ID="ID_889535113" MODIFIED="1309006308340" TEXT="o progressbare">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1309005492439" ID="ID_1819705655" MODIFIED="1309006447135" TEXT="Obsirnejsi text z uvodu, z historie a udalostiach v progressbare."/>
+</node>
+<node CREATED="1309005365692" ID="ID_972628931" MODIFIED="1309006310796" TEXT="stanovy">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309005368574" ID="ID_229013794" MODIFIED="1309006313967" TEXT="organy zdruzenia">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309005374121" ID="ID_1248336523" MODIFIED="1309006316747" TEXT="sponzori a podporovatelia">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309005434739" ID="ID_389509771" MODIFIED="1309006319863" TEXT="pre media">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1309005539194" ID="ID_1153486534" MODIFIED="1309005571033" TEXT="loga, propagacne materialy, kontakty etc."/>
+</node>
+<node CREATED="1309005443809" ID="ID_1948424508" MODIFIED="1309006323417" TEXT="galeria">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309005447925" ID="ID_98966920" MODIFIED="1309006327023" TEXT="podporte nas">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1309005835657" ID="ID_1407372326" MODIFIED="1309005838629" TEXT="sponzoring"/>
+<node CREATED="1309005838996" ID="ID_1913184394" MODIFIED="1309006040643" TEXT="kupa predmetov a materialov"/>
+<node CREATED="1309005848380" ID="ID_1901080739" MODIFIED="1309005891780" TEXT="online propagacia"/>
+</node>
+<node CREATED="1309005456740" ID="ID_955178540" MODIFIED="1309006331002" TEXT="pridaj sa">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1309005822607" ID="ID_1045295562" MODIFIED="1309005826966" TEXT="clen zdruzenia"/>
+<node CREATED="1309005827228" ID="ID_1750853234" MODIFIED="1309005832918" TEXT="dobrovolny podporovatel"/>
+</node>
+</node>
+<node CREATED="1309005235381" ID="ID_345448947" MODIFIED="1309006241127" POSITION="right" TEXT="wiki">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309005237147" ID="ID_1087122563" MODIFIED="1309006244583" POSITION="right" TEXT="kontakt">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1309006057096" ID="ID_1891978788" MODIFIED="1309006262443" POSITION="left" TEXT="pre clenov">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1309006077687" ID="ID_1474519672" MODIFIED="1309006084657" TEXT="profil clena">
+<node CREATED="1309006152229" ID="ID_81125574" MODIFIED="1309006156059" TEXT="osobne udaje"/>
+<node CREATED="1309006156333" ID="ID_503787883" MODIFIED="1309006160203" TEXT="identifikacne cislo"/>
+<node CREATED="1309006160505" ID="ID_1819505326" MODIFIED="1309006166431" TEXT="prehlad clenskych prispevkov"/>
+</node>
+<node CREATED="1309006178397" ID="ID_1478931538" MODIFIED="1309006185789" TEXT="zoznam clenov a kontakty"/>
+</node>
+</node>
+</map>
