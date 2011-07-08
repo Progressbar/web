5154922ff5f998d9186260b32598e1b6a1af9057
progressbarsk <marek@keram.name>
2011-07-09 01:39:21 +0200

    stanovy

--- db/seeds/pages_progressbar.rb
+++ db/seeds/pages_progressbar.rb
@@ -1,24 +1,20 @@
 ::I18n.locale = :sk
 
+puts 'locale -> sk'
+
+p_stanovy = Page.find_by_title('Stanovy')
+if p_stanovy
+  p_stanovy.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/stanovy.html')))
+  p_stanovy.parts.last.update_attributes(:body => IO.read(Rails.root.join('db/templates/stanovy_sidebar.html')))
+  puts 'page stanovy updated'
+end
+
 p_kontakt = Page.find_by_title('Kontakt')
 
 if p_kontakt
-#  p_kontakt.deletable = true
-#  p_kontakt.save
-#  p_kontakt.destroy
-#  p_kontakt_pos = p_kontakt.position.to_i
-#end
-#
-#if p_kontakt.nil?
-#  p_kontakt = Page.create(:title => 'Kontakt', :deletable => false, :show_in_menu => true, :position => p_kontakt_pos)
   p_kontakt.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/kontakt.html')))
   p_kontakt.parts.last.update_attributes(:body => '')
-    
-#
-#  p_kontakt.parts.where({
-#      :title => 'Side Body',
-#      :body => '',
-#      :position => 1
-#    })
-  puts 'page Kontakt updated'
+  puts 'page kontakt updated'
 end
+
+puts 'progressbar pages seed finish'
