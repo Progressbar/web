0af42541e202219abf50872d8196912ea5119854
progressbarsk <marek@keram.name>
2011-07-09 19:32:42 +0200

    default pages seed

--- db/seeds/pages_progressbar.rb
+++ db/seeds/pages_progressbar.rb
@@ -17,4 +17,37 @@ if p_kontakt
   puts 'page kontakt updated'
 end
 
+p_organy = Page.find_by_title('Orgány združenia')
+
+if p_organy
+  p_organy.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/organy.html')))
+  p_organy.parts.last.update_attributes(:body => '')
+  puts 'page organy updated'
+end
+
+
+p_media = Page.find_by_title('Pre médiá')
+
+if p_media
+  p_media.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/media.html')))
+  p_media.parts.last.update_attributes(:body => '')
+  puts 'page media updated'
+end
+
+p_podporte = Page.find_by_title('Podporte nás')
+
+if p_podporte
+  p_podporte.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/podporte.html')))
+  p_podporte.parts.last.update_attributes(:body => '')
+  puts 'page podporte updated'
+end
+
+p_zapoj = Page.find_by_title('Pridaj sa')
+
+if p_zapoj
+  p_zapoj.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/zapoj.html')))
+  p_zapoj.parts.last.update_attributes(:body => '')
+  puts 'page Pridaj sa updated'
+end
+
 puts 'progressbar pages seed finish'
