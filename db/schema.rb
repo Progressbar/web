f758621db2e6f7972e7181d9c07fd8f9056185fc
progressbarsk <marek@keram.name>
2011-08-11 21:42:05 +0200

    some drunk update ;-)

--- db/schema.rb
+++ db/schema.rb
@@ -181,8 +181,8 @@ ActiveRecord::Schema.define(:version => 20110730150730) do
   create_table "page_translations", :force => true do |t|
     t.integer  "page_id"
     t.string   "locale"
-    t.string   "title"
     t.string   "custom_title"
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
