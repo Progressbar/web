6f3082035adc80aa00bc81bea71cb8acd6d73299
progressbarsk <marek@keram.name>
2011-08-19 20:06:55 +0200

    upgrade import css, preklady a obrazky

--- db/seeds/pages_progressbar.rb
+++ db/seeds/pages_progressbar.rb
@@ -1,4 +1,6 @@
 # default pages in en
+::I18n.locale = :en
+
 ids = Progressbar::Application::PAGES
 
 home_page = Page.find_by_id(ids[:HOME_PAGE_ID])
@@ -13,6 +15,9 @@ join_us_page = Page.find_by_id(ids[:JOIN_US_PAGE_ID])
 thank_you_contact_page = Page.find_by_id(ids[:THANK_YOU_CONTACT_PAGE_ID])
 projects_page = Page.find_by_id(ids[:PROJECTS_PAGE_ID])
 sponsors_page = Page.find_by_id(ids[:SPONSORS_PAGE_ID])
+p_oficiality = Page.find_by_title('Legal stuff')
+p_stanovy = Page.find_by_title('Statutes')
+p_media = Page.find_by_title('Media')
 
 unless wiki_page
   wiki_page = Page.create(:title => 'Wiki', :deletable => false, :show_in_menu => true, :link_url => 'http://wiki.progressbar.sk/', :position => Page.last.position.to_i + 1)
@@ -21,19 +26,108 @@ end
 
 unless colophon_page
   colophon_page = Page.create(:title => 'Colophon', :deletable => false, :show_in_menu => false, :position => Page.last.position.to_i + 1)
+  colophon_page.parts.create({
+      :title => 'Body',
+      :body => '',
+      :position => 0
+    })
+  
+  colophon_page.parts.create({
+      :title => 'Side Body',
+      :body => '',
+      :position => 1
+    })
+  
   puts "colophon #{colophon_page.id}"
 end
 
 unless join_us_page
   join_us_page = Page.create(:title => 'Join Us', :deletable => false, :show_in_menu => false, :position => Page.last.position.to_i + 1)
+  join_us_page.parts.create({
+      :title => 'Body',
+      :body => IO.read(Rails.root.join('db/templates/pridaj.html')),
+      :position => 0
+    })
+  
+  join_us_page.parts.create({
+      :title => 'Side Body',
+      :body => '',
+      :position => 1
+    })  
   puts "join us #{join_us_page.id}"
 end
 
 unless projects_page
   projects_page = Page.create(:title => 'Projects', :deletable => false, :show_in_menu => true, :draft => true, :link_url => 'http://wiki.progressbar.sk/projects/', :position => Page.last.position.to_i + 1)
+  projects_page.parts.create({
+      :title => 'Body',
+      :body => '',
+      :position => 0
+    })
+  
+  projects_page.parts.create({
+      :title => 'Side Body',
+      :body => '',
+      :position => 1
+    })
   puts "projects #{projects_page.id}"
 end
-    
+
+unless p_oficiality
+  p_oficiality = Page.create(:title => 'Legal stuff', :deletable => false, :show_in_menu => true, :skip_to_first_child => true, :position => Page.last.position.to_i + 1)
+  
+  puts "page Oficiality was created: #{p_oficiality.id}" 
+end
+
+unless p_stanovy
+  p_stanovy = p_oficiality.children.create(:title => 'Statutes', :deletable => false, :show_in_menu => true, :position => Page.last.position.to_i + 1)
+  p_stanovy.parts.create({
+      :title => 'Body',
+      :body => IO.read(Rails.root.join('db/templates/stanovy.html')),
+      :position => 0
+    })
+  
+  p_stanovy.parts.create({
+      :title => 'Side Body',
+      :body => IO.read(Rails.root.join('db/templates/stanovy_sidebar.html')),
+      :position => 1
+    })
+end
+
+unless p_media
+  p_media = p_oficiality.children.create(:title => 'Media', :deletable => false, :show_in_menu => true, :position => Page.last.position.to_i + 1)
+  p_media.parts.create({
+      :title => 'Body',
+      :body => '',
+      :position => 0
+    })
+  
+  p_media.parts.create({
+      :title => 'Side Body',
+      :body => '',
+      :position => 1
+    })
+  
+  puts "p_media #{p_media.id}"
+end
+
+unless sponsors_page
+  sponsors_page = p_oficiality.children.create(:title => 'Sponsors', :deletable => false, :show_in_menu => true, :position => Page.last.position.to_i + 1)
+  sponsors_page.parts.create({
+      :title => 'Body',
+      :body => '',
+      :position => 0
+    })
+  
+  sponsors_page.parts.create({
+      :title => 'Side Body',
+      :body => '',
+      :position => 1
+    })
+  
+  puts "sponsors_page #{sponsors_page.id}"
+end
+
 ::I18n.locale = :sk
 puts 'locale -> sk'
 
@@ -49,95 +143,20 @@ colophon_page.update_attributes(:title => 'Tiráž')
 join_us_page.update_attributes(:title => 'Pridaj sa')
 projects_page.update_attributes(:title => 'Projekty')
 
-p_oficiality = Page.find_by_title('Oficiality')
-p_stanovy = Page.find_by_title('Stanovy')
-p_media = Page.find_by_title('Pre médiá')
+p_oficiality.update_attributes(:title => 'Oficiality')
+p_stanovy.update_attributes(:title => 'Stanovy')
+p_media.update_attributes(:title => 'Pre médiá')
+sponsors_page.update_attributes(:title => 'Sponzori')
 
-unless p_oficiality
-  p_oficiality = Page.create(:title => 'Oficiality', :deletable => false, :show_in_menu => true, :skip_to_first_child => true, :position => Page.last.position.to_i + 1)
-  
-  puts "page Oficiality was created: #{p_oficiality.id}" 
-end
 
-if p_stanovy
-  p_stanovy.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/stanovy.html')))
-  p_stanovy.parts.last.update_attributes(:body => IO.read(Rails.root.join('db/templates/stanovy_sidebar.html')))
-  puts 'page stanovy updated'
-else
-  p_stanovy = p_oficiality.children.create(:title => 'Stanovy', :deletable => false, :show_in_menu => true, :position => Page.last.position.to_i + 1)
-  p_stanovy.parts.create({
-      :title => 'Body',
-      :body => IO.read(Rails.root.join('db/templates/stanovy.html')),
-      :position => 0
-    })
-  
-  p_stanovy.parts.create({
-      :title => 'Side Body',
-      :body => IO.read(Rails.root.join('db/templates/stanovy_sidebar.html')),
-      :position => 1
-    })
-  
-  puts 'page Stanovy was created'
-end
+p_stanovy.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/stanovy.html')))
+p_stanovy.parts.last.update_attributes(:body => IO.read(Rails.root.join('db/templates/stanovy_sidebar.html')))
 
-if join_us_page && join_us_page.parts.first
-  join_us_page.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/pridaj.html')))
-  join_us_page.parts.last.update_attributes(:body => '')
-else
-  join_us_page.parts.create({
-      :title => 'Body',
-      :body => IO.read(Rails.root.join('db/templates/pridaj.html')),
-      :position => 0
-    })
-  
-  join_us_page.parts.create({
-      :title => 'Side Body',
-      :body => '',
-      :position => 1
-    })  
-end
+join_us_page.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/pridaj.html')))
 
-if p_media
-  p_media.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/media.html')))
-  p_media.parts.last.update_attributes(:body => '')
-  puts 'page Pre media updated'
-else
-  p_media = p_oficiality.children.create(:title => 'Pre médiá', :deletable => false, :show_in_menu => true, :position => Page.last.position.to_i + 1)
-  p_media.parts.create({
-      :title => 'Body',
-      :body => IO.read(Rails.root.join('db/templates/media.html')),
-      :position => 0
-    })
-  
-  p_media.parts.create({
-      :title => 'Side Body',
-      :body => '',
-      :position => 1
-    })
-  
-  puts 'page Pre media was created'
-end
+p_media.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/media.html')))
 
-if sponsors_page
-  sponsors_page.parts.first.update_attributes(:body => '')
-  sponsors_page.parts.last.update_attributes(:body => '')
-  puts 'page Sponsors updated'
-else
-  sponsors_page = p_oficiality.children.create(:title => 'Sponzory', :deletable => false, :show_in_menu => true, :position => Page.last.position.to_i + 1)
-  sponsors_page.parts.create({
-      :title => 'Body',
-      :body => '',
-      :position => 0
-    })
-  
-  sponsors_page.parts.create({
-      :title => 'Side Body',
-      :body => '',
-      :position => 1
-    })
-  
-  puts "sponsors_page #{sponsors_page.id}"
-end
+sponsors_page.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/sponzori.html')))
 
 if contact_page
   contact_page.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/kontakt.html')))
@@ -197,31 +216,6 @@ if contact_page
     puts 'page Kontakt Fotonavigácia was created'
   end
 
-  #
-  #if contact_page_qr
-  #  contact_page_qr.update_attribute(:deletable, false)
-  #  contact_page_qr.update_attribute(:show_in_menu, true)
-  #  
-  #  contact_page_qr.parts.first.update_attribute(:body, IO.read(Rails.root.join('db/templates/kontakt_qr.html')))
-  #  
-  #  puts 'page Kontakt QR was updated'
-  #else
-  #  contact_page_qr = contact_page.children.create(:title => 'QR', :deletable => false, :show_in_menu => true, :position => Page.last.position.to_i + 1)
-  #  contact_page_qr.parts.create({
-  #      :title => 'Body',
-  #      :body => IO.read(Rails.root.join('db/templates/kontakt_qr.html')),
-  #      :position => 0
-  #    })
-  #  
-  #  contact_page_qr.parts.create({
-  #      :title => 'Side Body',
-  #      :body => '',
-  #      :position => 1
-  #    })
-  #  
-  #  puts 'page Kontakt QR was created'
-  #end
-
   if contact_page_mapy
     contact_page_mapy.update_attribute(:deletable, false)
     contact_page_mapy.update_attribute(:show_in_menu, true)
@@ -246,32 +240,6 @@ if contact_page
     puts 'page Kontakt Mapy was created'
   end
 end
-
-#p_organy = Page.find_by_title('Orgány združenia')
-#
-#if p_organy
-#  p_organy.update_attributes(:draft => true)
-#  p_organy.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/organy.html')))
-#  p_organy.parts.last.update_attributes(:body => '')
-#  puts 'page organy updated'
-#end
-
-#p_podporte = Page.find_by_title('Podporte nás')
-#
-#if p_podporte
-#  p_podporte.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/podporte.html')))
-#  p_podporte.parts.last.update_attributes(:body => '')
-#  puts 'page podporte updated'
-#end
-
-#p_pridaj = Page.find_by_title('Pridaj sa')
-#
-#if p_pridaj
-#  p_pridaj.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/pridaj.html')))
-#  p_pridaj.parts.last.update_attributes(:body => '')
-#  puts 'page Pridaj sa updated'
-#end
-#
 
 puts 'reorder progressbar pages'
 
