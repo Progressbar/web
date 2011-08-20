33e919d22c319deab1bb0e8b899d384e27267f0f
progressbarsk <marek@keram.name>
2011-08-20 23:22:04 +0200

    komplex update and cleaning

--- db/seeds/pages_sponsored.rb
+++ db/seeds/pages_sponsored.rb
@@ -2,7 +2,7 @@
 
 p_sponsored = Page.find_by_title('Sponsored')
 
-p_webdevelopment = Page.find_by_title('Web aplikácie')
+p_webdevelopment = Page.find_by_title('Web development')
 p_pentests = Page.find_by_title('Penetračné testy')
 p_outsourcing = Page.find_by_title('Outsourcing správy')
 p_webhosting = Page.find_by_title('Web hosting')
@@ -12,7 +12,7 @@ unless p_sponsored
 end
 
 unless p_webdevelopment
-  p_webdevelopment = p_sponsored.children.create(:title => 'Web aplikácie', :deletable => false, :show_in_menu => false, :position => Page.last.position.to_i + 1)
+  p_webdevelopment = p_sponsored.children.create(:title => 'Web development', :deletable => false, :show_in_menu => false, :position => Page.last.position.to_i + 1)
   p_webdevelopment.parts.create({
       :title => 'Body',
       :body => IO.read(Rails.root.join('db/templates/sponsored/web-development.html')),
@@ -27,6 +27,23 @@ unless p_webdevelopment
   
 else
   p_webdevelopment.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/sponsored/web-development.html')))
+end
+unless p_pentests
+  p_pentests = p_sponsored.children.create(:title => 'Penetračné testy', :deletable => false, :show_in_menu => false, :position => Page.last.position.to_i + 1)
+  p_pentests.parts.create({
+      :title => 'Body',
+      :body => IO.read(Rails.root.join('db/templates/sponsored/pentests.html')),
+      :position => 0
+    })
+  
+  p_pentests.parts.create({
+      :title => 'Side Body',
+      :body => '',
+      :position => 1
+    })
+  
+else
+  p_pentests.parts.first.update_attributes(:body => IO.read(Rails.root.join('db/templates/sponsored/pentests.html')))
 end
 
 puts 'sponsored pages updated'
