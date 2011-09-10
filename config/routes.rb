cbb0fb7c73849b2d45710601b5c3339122f8d5ac
progressbarsk <marek@keram.name>
2011-09-10 21:46:08 +0200

    test email newsletter

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Progressbar::Application.routes.draw do
   match '/pridaj-sa' => 'registrations#index'
   match '/join-us' => 'registrations#index'
   
+  resources :newsletter
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
