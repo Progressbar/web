80a38e5bc6cc3ac71e3d29d0743410fd7f4045b6
progressbarsk <marek@keram.name>
2011-09-19 20:23:59 +0200

    cleaning

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+  :tls => true,
+  :address => "smtp.gmail.com",
+  :port => "587",
+  :domain => "localhost",
+  :authentication => :plain,
+  :user_name => "keraml@gmail.com",
+  :password => "marek_radecki"
+}
+
 Progressbar::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -12,6 +22,7 @@ Progressbar::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
+
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
@@ -19,7 +30,7 @@ Progressbar::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :info
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
