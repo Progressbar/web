a6f5f94d0e036a783d7593ab24cd17c2ceb3b869
progressbarsk <marek@keram.name>
2011-10-06 22:06:45 +0200

    share buttons test refactoring

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -104,8 +104,8 @@ $(function() {
                 },
                 error: function( xhr, status, index, anchor ) {
                     $( anchor.hash ).html(
-                        "Couldn't load this tab. We'll try to fix this as soon as possible."
-                        );
+                    "Couldn't load this tab. We'll try to fix this as soon as possible."
+                );
                 }
             }
         });
@@ -117,8 +117,8 @@ $(function() {
 
     $("#feedback-btn").bind('click', function () {
         $.fancybox(
-            '<iframe src="https://spreadsheets.google.com/spreadsheet/embeddedform?formkey=dHBPbTMwOHBvMzZVLVEzM09wanh0WEE6MQ" width="640" height="570" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
-            );
+        '<iframe src="https://spreadsheets.google.com/spreadsheet/embeddedform?formkey=dHBPbTMwOHBvMzZVLVEzM09wanh0WEE6MQ" width="640" height="570" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
+    );
         return false;
     });
 
@@ -128,10 +128,10 @@ $(function() {
             var p = document.body,
             s = null;
             var libs = [
-            'https://platform.twitter.com/widgets.js',
-            'https://platform.linkedin.com/in.js',
-            'https://connect.facebook.net/en_US/all.js#xfbml=1',
-            'https://apis.google.com/js/plusone.js'
+                'https://platform.twitter.com/widgets.js',
+                'https://platform.linkedin.com/in.js',
+                'https://connect.facebook.net/en_US/all.js#xfbml=1',
+                'https://apis.google.com/js/plusone.js'
             ];
 
             while (libs.length) {
@@ -142,227 +142,211 @@ $(function() {
 
                 p.appendChild(s);
             }
-            
+
             evil_libs_loaded = true;
-        }        
-    }
-    
-    if ($('#share_this_event').length > 0) {
-        load_evils();
-    }
-    
-    $("#share-btn").bind('click', function () {
-        if ($('#share').length == 0) {
-            $('<div style="display:none;"><div id="share">' +
-                '<span class="share-btn"><g:plusone size="tall"></g:plusone></span>' +
-                '<span class="share-btn"><a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="progressbarsk">Tweet</a></span>' +
-                '<span class="share-btn"><script type="IN/Share" data-counter="top"></script></span>' +
-                '<span class="share-btn"><fb:like href="" send="false" layout="box_count" width="60" height="64" show_faces="false" action="like" font="arial"></fb:like></span>' +
-                '<p style="padding-left: 8px; padding-top: 8px;">Ďakujeme, že o nás dávate vedieť. <br />Thank you gave to know about us.</p>' +
-                '</div></div>').appendTo(body);
         }
-        $.fancybox($('#share').html(),  {
-                'autoDimensions' : false,
-                'width'          : 350,
-                'height'         : 150
-        });
-        
-        load_evils();
+    }
 
-        return false;
-    });
+    $('#feedback').html('<div id="share">' +
+        '<span class="share-btn"><g:plusone size="tall"></g:plusone></span>' +
+        '<span class="share-btn"><a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="progressbarsk">Tweet</a></span>' +
+        '<span class="share-btn"><script type="IN/Share" data-counter="top"></script></span>' +
+        '<span class="share-btn"><fb:like href="" send="false" layout="box_count" width="60" height="64" show_faces="false" action="like" font="arial"></fb:like></span>' +
+        '</div>');
 
+    
+    load_evils();
 
-    $('a.todo').bind('click',
-        function () {
-            $.fancybox(
-                '<h2>TODO</h2><p>Obsah tejto stránky čaká na teba. ;-)<br />' +
-                '<a href="https://github.com/Progressbar/web">Prejsť na Github</a><br />' +
-                '<span class="note">Content for this section is not currently available.</span>' +
-                '</p>',
-                {
-                    'autoDimensions' : false,
-                    'width'          : 350,
-                    'height'         : 'auto'
-                }
-                );
-            return false;
-        });
+$('a.todo').bind('click',
+function () {
+    $.fancybox(
+    '<h2>TODO</h2><p>Obsah tejto stránky čaká na teba. ;-)<br />' +
+        '<a href="https://github.com/Progressbar/web">Prejsť na Github</a><br />' +
+        '<span class="note">Content for this section is not currently available.</span>' +
+        '</p>',
+    {
+        'autoDimensions' : false,
+        'width'          : 350,
+        'height'         : 'auto'
+    }
+);
+    return false;
+});
 
-    $('#order a').not('.todo').fancybox();
+$('#order a').not('.todo').fancybox();
 
-    var kCalendar = function (holder) {
-        var calendar_data = 'test';
+var kCalendar = function (holder) {
+    var calendar_data = 'test';
 
-        var kCalendar = {
-            datepicker : '',
-            dialog : '',
-            active : null,
-            current_date : '',
-            initialized : false,
-            firstDay : 1,
-            data : [],
-            year : 2011,
-            month : 1,
-            defaultDate : null,
+    var kCalendar = {
+        datepicker : '',
+        dialog : '',
+        active : null,
+        current_date : '',
+        initialized : false,
+        firstDay : 1,
+        data : [],
+        year : 2011,
+        month : 1,
+        defaultDate : null,
 
-            onChangeMonthYear : function (year, month, inst) {
-                inst.settings.year = year;
-                inst.settings.month = month;
-                inst.settings.datepicker.datepicker('destroy');
-                inst.settings.initialized = false;
-                inst.settings.defaultDate = new Date(year, month -1);
-                inst.settings.loadData(year, month);
-            },
+        onChangeMonthYear : function (year, month, inst) {
+            inst.settings.year = year;
+            inst.settings.month = month;
+            inst.settings.datepicker.datepicker('destroy');
+            inst.settings.initialized = false;
+            inst.settings.defaultDate = new Date(year, month -1);
+            inst.settings.loadData(year, month);
+        },
 
-            beforeShowDay : function (date) { },
+        beforeShowDay : function (date) { },
 
-            onSelect: function(dateText, inst) {
-                var date = new Date(dateText);
-                var tmp_key = date.getDate().toString() + date.getMonth().toString() + date.getFullYear().toString();
-                var calendar = inst.settings,
-                dialog = calendar.dialog,
-                picker = calendar.datepicker,
-                day = null,
-                picker_pos = picker.offset(),
-                dialog_pos = [picker_pos.left - picker.width() - 15, picker_pos.top - $(window).scrollTop()],
-                body = '';
+        onSelect: function(dateText, inst) {
+            var date = new Date(dateText);
+            var tmp_key = date.getDate().toString() + date.getMonth().toString() + date.getFullYear().toString();
+            var calendar = inst.settings,
+            dialog = calendar.dialog,
+            picker = calendar.datepicker,
+            day = null,
+            picker_pos = picker.offset(),
+            dialog_pos = [picker_pos.left - picker.width() - 15, picker_pos.top - $(window).scrollTop()],
+            body = '';
 
-                if (calendar_data[tmp_key]) {
-                    day = calendar_data[tmp_key];
-                    dialog.html('');
-                    for (i = 0; i < day.length; i++) {
-                        var ev = day[i];
-                        var holder = $('<div style="clear:both;">').appendTo(dialog);
+            if (calendar_data[tmp_key]) {
+                day = calendar_data[tmp_key];
+                dialog.html('');
+                for (i = 0; i < day.length; i++) {
+                    var ev = day[i];
+                    var holder = $('<div style="clear:both;">').appendTo(dialog);
 
-                        $('<a>', {
-                            'href' : '/events/' + ev.id,
-                            'class' : 'float-left',
-                            'text' : ev.title.length > 25 ? $.trim(ev.title.substring(0, 25)) + '..' : ev.title
+                    $('<a>', {
+                        'href' : '/events/' + ev.id,
+                        'class' : 'float-left',
+                        'text' : ev.title.length > 25 ? $.trim(ev.title.substring(0, 25)) + '..' : ev.title
+                    }).appendTo(holder);
+
+                    if (ev.start_at.getDayOfYear() === ev.end_at.getDayOfYear()) {
+                        $('<span>', {
+                            'class' : 'float-right',
+                            'text' : ev.start_at.getHours() + ':' + $.strPad(ev.start_at.getMinutes(), 2, 0) + ' - ' + ev.end_at.getHours() + ':' + $.strPad(ev.end_at.getMinutes(), 2, 0)
                         }).appendTo(holder);
-
-                        if (ev.start_at.getDayOfYear() === ev.end_at.getDayOfYear()) {
-                            $('<span>', {
-                                'class' : 'float-right',
-                                'text' : ev.start_at.getHours() + ':' + $.strPad(ev.start_at.getMinutes(), 2, 0) + ' - ' + ev.end_at.getHours() + ':' + $.strPad(ev.end_at.getMinutes(), 2, 0)
-                            }).appendTo(holder);
-                        }
                     }
                 }
+            }
 
-                if (calendar.active === dateText) {
-                    dialog.dialog('close');
-                    calendar.active = null;
-                } else {
+            if (calendar.active === dateText) {
+                dialog.dialog('close');
+                calendar.active = null;
+            } else {
 
-                    dialog.dialog({
-                        'id' : 2,
-                        'draggable' : true,
-                        'resizable' : false,
-                        'position' : dialog_pos,
-                        'title' : 'Program: ' + date.toDateString()
-                    });
-                    calendar.active = dateText;
-                }
-            },
+                dialog.dialog({
+                    'id' : 2,
+                    'draggable' : true,
+                    'resizable' : false,
+                    'position' : dialog_pos,
+                    'title' : 'Program: ' + date.toDateString()
+                });
+                calendar.active = dateText;
+            }
+        },
 
-            initPicker : function () {
-                var that = this;
+        initPicker : function () {
+            var that = this;
 
-                this.datepicker.datepicker(this);
-            },
+            this.datepicker.datepicker(this);
+        },
 
-            loadData : function (year, month) {
-                var that = this;
+        loadData : function (year, month) {
+            var that = this;
 
-                $.ajax({
-                    url: '/events/archive/' + year + '/' + month,
-                    dataType: 'json',
-                    type: 'GET',
-                    success: function (response) {
-                        if (response) {
-                            var data = {},
-                            tmp_start_date = null,
-                            tmp_end_date = null,
-                            tmp_key = null,
-                            tmp_date = null;
-                            try  {
-                                for (i = 0; i < response.length; i++) {
-                                    tmp_start_date = new Date(response[i]['event']['start_at']);
-                                    tmp_start_date = new Date(tmp_start_date.getFullYear(), tmp_start_date.getMonth(), tmp_start_date.getDate(), tmp_start_date.getHours() - 2, tmp_start_date.getMinutes());
-                                    response[i]['event']['start_at'] = tmp_start_date;
-                                    tmp_end_date = new Date(response[i]['event']['end_at']);
-                                    tmp_end_date = new Date(tmp_end_date.getFullYear(), tmp_end_date.getMonth(), tmp_end_date.getDate(), tmp_end_date.getHours() - 2, tmp_end_date.getMinutes());
-                                    response[i]['event']['end_at'] = tmp_end_date;
-                                    
-                                    var diff_days = tmp_end_date.getDayOfYear() - tmp_start_date.getDayOfYear();
+            $.ajax({
+                url: '/events/archive/' + year + '/' + month,
+                dataType: 'json',
+                type: 'GET',
+                success: function (response) {
+                    if (response) {
+                        var data = {},
+                        tmp_start_date = null,
+                        tmp_end_date = null,
+                        tmp_key = null,
+                        tmp_date = null;
+                        try  {
+                            for (i = 0; i < response.length; i++) {
+                                tmp_start_date = new Date(response[i]['event']['start_at']);
+                                tmp_start_date = new Date(tmp_start_date.getFullYear(), tmp_start_date.getMonth(), tmp_start_date.getDate(), tmp_start_date.getHours() - 2, tmp_start_date.getMinutes());
+                                response[i]['event']['start_at'] = tmp_start_date;
+                                tmp_end_date = new Date(response[i]['event']['end_at']);
+                                tmp_end_date = new Date(tmp_end_date.getFullYear(), tmp_end_date.getMonth(), tmp_end_date.getDate(), tmp_end_date.getHours() - 2, tmp_end_date.getMinutes());
+                                response[i]['event']['end_at'] = tmp_end_date;
 
-                                    for (var j = 0; j <= diff_days;j++) {
-                                        tmp_date = new Date(tmp_start_date.getFullYear(), tmp_start_date.getMonth(), tmp_start_date.getDate() + j);
-                                        tmp_key = tmp_date.getDate().toString() + tmp_date.getMonth().toString() + tmp_date.getFullYear().toString();
-                                        data[tmp_key] =  data[tmp_key] || [];
-                                        if (data[tmp_key].indexOf(response[i]['event']) === -1) {
-                                            data[tmp_key].push(response[i]['event']);
-                                        }
+                                var diff_days = tmp_end_date.getDayOfYear() - tmp_start_date.getDayOfYear();
+
+                                for (var j = 0; j <= diff_days;j++) {
+                                    tmp_date = new Date(tmp_start_date.getFullYear(), tmp_start_date.getMonth(), tmp_start_date.getDate() + j);
+                                    tmp_key = tmp_date.getDate().toString() + tmp_date.getMonth().toString() + tmp_date.getFullYear().toString();
+                                    data[tmp_key] =  data[tmp_key] || [];
+                                    if (data[tmp_key].indexOf(response[i]['event']) === -1) {
+                                        data[tmp_key].push(response[i]['event']);
                                     }
                                 }
+                            }
 
-                                calendar_data = data;
+                            calendar_data = data;
 
-                                if (!that.initialized) {
-                                    that.initialized = true;
-                                    that.initPicker();
-                                }
-                            } catch (err) {
-                                if (!!console) {
-                                    console.log(err);
-                                }
+                            if (!that.initialized) {
+                                that.initialized = true;
+                                that.initPicker();
+                            }
+                        } catch (err) {
+                            if (!!console) {
+                                console.log(err);
                             }
                         }
                     }
-                });
-            },
+                }
+            });
+        },
 
 
 
-            init: function(holder) {
-                var that = this;
+        init: function(holder) {
+            var that = this;
 
-                this.datepicker = $('<div id="jquery-ui-calendar" />').prependTo(holder);
-                this.dialog = $('<div id="jquery-ui-calendar-dialog" style="display:none;text-align: left;" />').prependTo(holder);
-                //
-                this.datepicker.dialog = this.dialog;
+            this.datepicker = $('<div id="jquery-ui-calendar" />').prependTo(holder);
+            this.dialog = $('<div id="jquery-ui-calendar-dialog" style="display:none;text-align: left;" />').prependTo(holder);
+            //
+            this.datepicker.dialog = this.dialog;
 
-                this.current_date = new Date();
-                this.year = this.current_date.getFullYear();
-                this.month = this.current_date.getMonth() + 1;
-                this.defaultDate = this.current_date;
+            this.current_date = new Date();
+            this.year = this.current_date.getFullYear();
+            this.month = this.current_date.getMonth() + 1;
+            this.defaultDate = this.current_date;
 
-                this.beforeShowDay = function (date) {
-                    var cls = '',
-                    rt = false,
-                    tmp_key = date.getDate().toString() + date.getMonth().toString() + date.getFullYear().toString();
+            this.beforeShowDay = function (date) {
+                var cls = '',
+                rt = false,
+                tmp_key = date.getDate().toString() + date.getMonth().toString() + date.getFullYear().toString();
 
-                    if ( calendar_data[tmp_key] ) {
-                        cls += ' ui-datepicker-event';
-                        rt = true;
-                    }
-
-                    if (date < that.current_date) {
-                        cls += ' ui-datepicker-past';
-                    }
-
-                    return [rt, $.trim(cls)];
+                if ( calendar_data[tmp_key] ) {
+                    cls += ' ui-datepicker-event';
+                    rt = true;
                 }
 
-                this.loadData(this.year, this.month );
+                if (date < that.current_date) {
+                    cls += ' ui-datepicker-past';
+                }
 
-                return this;
+                return [rt, $.trim(cls)];
             }
+
+            this.loadData(this.year, this.month );
+
+            return this;
         }
+    }
 
-        return holder.length > 0 ? kCalendar.init(holder) : false;
-    };
+    return holder.length > 0 ? kCalendar.init(holder) : false;
+};
 
-    var myCalendar = kCalendar($('#jquery-ui-calendar-holder'));
+var myCalendar = kCalendar($('#jquery-ui-calendar-holder'));
 });
