4921e8c1e6b3b5bcda24d5b6570b4ded5de7a192
progressbarsk <marek@keram.name>
2012-05-12 18:18:02 +0200

    delete old stuff for new version from scratch

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Progressbar::Application.config.secret_token = '1a7faf9e58b26543a8f01dc5f243c1c8e3bddc2660a637efd8c9c946c5a13d87c31c8227b67fc8c882dac3d994c8796b520902ac47ecae0677dedbc9101fc48f'
--- db/migrate/20110709124333_add_custom_url_field_to_blog_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddCustomUrlFieldToBlogPosts < ActiveRecord::Migration
-  def self.up
-    add_column :blog_posts, :custom_url, :string
-  end
-
-  def self.down
-    remove_column :blog_posts, :custom_url
-  end
-end
