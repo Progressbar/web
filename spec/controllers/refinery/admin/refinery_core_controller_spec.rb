516f135dac6e5054639521e33321c8adf2962851
progressbarsk <marek@keram.name>
2012-07-29 13:19:20 +0200

    prepare for testing

---
+++ spec/controllers/refinery/admin/refinery_core_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+module Refinery
+  module Admin
+    describe CoreController do
+      login_refinery_user
+
+      it "updates the plugin positions" do
+       
+      end
+    end
+  end
+end
