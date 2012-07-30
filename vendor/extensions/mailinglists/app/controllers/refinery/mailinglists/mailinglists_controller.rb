47e5d30fbd957b2eb944c37d9a9c29b1eb4e06fe
progressbarsk <keram@trip.sk>
2012-07-30 18:36:13 +0200

    fixed errors in mailing list extension

--- vendor/extensions/mailinglists/app/controllers/refinery/mailinglists/mailinglists_controller.rb
+++ vendor/extensions/mailinglists/app/controllers/refinery/mailinglists/mailinglists_controller.rb
@@ -20,33 +20,25 @@ module Refinery
       def create
         @mailinglist = Mailinglist.new(params[:mailinglist])
         subscriber = Mailinglist.find_by_email(@mailinglist[:email])
-        # if subscriber not exits we create new and subcribe him where they want
+        # if subscriber not exits we create new and subcribe him where he want
         subscribed_general = false
         subscribed_events = false
-        if subscriber.nil? and @mailinglist.valid?
-          subscribed_general = subscribe(subscriber, 'general') if @mailinglist.general
-          subscribed_events = subscribe(subscriber, 'events') if @mailinglist.events
+        if subscriber.nil?
+          subscribed_general = subscribe(@mailinglist, 'general') if @mailinglist[:general]
+          subscribed_events = subscribe(@mailinglist, 'events') if @mailinglist[:events]
 
-          if subscribed_general or subscribed_events
-            if @mailinglist.save
-              begin
-                Mailer.notification(@mailinglist, request).deliver
-              rescue => e
-                logger.warn "There was an error delivering the mailinglist notification.\n#{e.message}\n"
-              end
-            end
-          end
+          Mailer.notification(@mailinglist, request).deliver if (subscribed_general or subscribed_events)
         else
-          # otherwise we try subscribe or unsubscribe him from what they want
+          # otherwise we try subscribe or unsubscribe him from what he want
           if @mailinglist[:general]
             if subscriber[:general]
-              unsubscribe(subscriber, 'general')
+              unsubscribe(subscriber, 'general') and return
             else
               subscribed_general = subscribe(subscriber, 'general')
             end
           elsif @mailinglist[:events]
             if subscriber[:events]
-              unsubscribe(subscriber, 'events')
+              unsubscribe(subscriber, 'events') and return
             else
               subscribed_events = subscribe(subscriber, 'events')
             end
@@ -57,31 +49,38 @@ module Refinery
           flash[:success] = 'Subscription success.'
           redirect_to refinery.thank_you_mailinglists_mailinglists_path
         else
-          flash[:error] = 'Houston we have a problem.'
-          render :action => 'new'
+          raise 'Something is bad.'
         end
+
+        rescue
+          logger.warn "There was an error with email (#{@mailinglist.email}). \n#{$!.message}\n"
+          flash[:error] = "Houston we have a problem. If problem persist please contact us on email: #{Refinery::Setting.get(:site_email)} ."
+          render :action => 'new'
       end
 
       protected
 
       def unsubscribe(subscriber, mailinglist)
         subscriber.update_attributes(mailinglist.to_sym => false)
-        redirect_to Refinery::Setting.get(":#{mailinglist}_mailing_list_unsubscribe_url".to_sym)
+        redirect_to Refinery::Setting.get("#{mailinglist}_mailing_list_unsubscribe_url".to_sym)
       end
 
       def subscribe(subscriber, mailinglist)
-        subscriber.update_attributes(mailinglist.to_sym => true)
-        self.send_subscribe_request(Refinery::Setting.get(":#{mailinglist}_mailing_list_subscribe_url".to_sym)
+        subscribed = self.send_subscribe_request(Refinery::Setting.get("#{mailinglist}_mailing_list_subscribe_url".to_sym))
+        subscriber.update_attributes(mailinglist.to_sym => true) if subscribed
+        subscribed
       end
 
       def send_subscribe_request(url)
+        response = false
         begin
-          response = Net::HTTP.post_form(URI.parse(url), {'email' => @mailinglist.email})
+          request = Net::HTTP.post_form(URI.parse(url), {'email' => @mailinglist.email})
+          response = true
         rescue => e
           logger.warn "There was an error subscribe email (#{@mailinglist.email}) to mailinglist (#{url}). \n#{e.message}\n"
         end
 
-        response && response.code == 200
+        response
       end
 
       def find_page
