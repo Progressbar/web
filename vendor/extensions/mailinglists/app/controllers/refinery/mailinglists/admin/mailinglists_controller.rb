e20d33139e90a9e4e36fe2c732de01301ac17beb
progressbarsk <keram@trip.sk>
2012-07-30 17:17:55 +0200

    cleaning mess in mailing list controller ;(

--- vendor/extensions/mailinglists/app/controllers/refinery/mailinglists/admin/mailinglists_controller.rb
+++ vendor/extensions/mailinglists/app/controllers/refinery/mailinglists/admin/mailinglists_controller.rb
@@ -14,19 +14,8 @@ module Refinery
             search_all_mailinglists
           end
 
-    puts 'xxxxxxxxx-------'
-    x = ::Refinery::Plugins.active.collect {|p|
-      p
-    }
-
-          puts ::Refinery::Plugins.active.names
-#          puts ::Refinery::Plugins.active.first[:title]
-    puts 'xxxxxxxxx'
-
           @grouped_mailinglists = group_by_date(@mailinglists)
         end
-
-
 
       end
     end
