c5a726b16fb1a76888f043492fff826cdf4c6d6d
progressbarsk <keram@trip.sk>
2012-08-10 18:05:39 +0200

    update homepage specs

--- spec/requests/refinery/frontend/home_spec.rb
+++ spec/requests/refinery/frontend/home_spec.rb
@@ -5,21 +5,34 @@ require "spec_helper"
 module Refinery
 
   describe "Progressbar:Homepage" do
+    before do
+      visit '/'
+    end
 
-    context "as anonymous users" do
+    context "as anonymous user visiting homepage" do
 
-      it "should introduce progressbar" do
-        visit '/'
-
+      it "should have title: Úvod .." do
         page.should have_selector('title', :content => 'Úvod - Progressbar Hackerspace')
+      end
 
+      it "should have logo with site name" do
         within "#logo" do
           page.should have_content(Refinery::Core.site_name)
         end
+      end
 
+      it "should introduce progressbar" do
         within "#promo-content" do
           # Vitajte na stránkach prvého slovenského <br /> <a href="http://en.wikipedia.org/wiki/Hackerspace">hackerspace</a> <strong>progressbar.sk</strong>.
           page.should have_content('Vitajte na stránkach prvého slovenského') 
+        end
+      end
+
+      it "should have top navigation links" do
+        within "#nav-holder" do
+          page.should have_selector("a[href='/pridaj-sa']") # Registration page
+          page.should have_selector("a[href='/refinery/']") # Member section
+          page.should have_selector("a[href='/en']") # English version
         end
       end
 
