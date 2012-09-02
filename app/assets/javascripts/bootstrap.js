80b04c2d40c5ebf9e99633fd995f854c6386a5ee
progressbarsk <keram@trip.sk>
2012-09-02 13:59:57 +0200

    cleaning

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,32 +1,19 @@
-$.strPad = function(i,l,s) {
-    var o = i.toString();
-    if (!s) {
-        s = '0';
-    }
-    while (o.length < l) {
-        o = s + o;
-    }
-    return o;
-};
-
-Date.prototype.getDayOfYear = function() {
-    return Math.ceil((this - new Date(this.getFullYear(),0,1)) / 86400000);
-}
-
 $(function() {
     if (!Modernizr.input.placeholder) {
         $('html').addClass('no-placeholder');
     }
 
-    $('a.scroll').bind('click', function () {
-        var a = $(this),
-        scroll_to = $(a.attr('href'));
-        if (scroll_to.length > 0) {
-            $.scrollTo(scroll_to, 1000);
-        }
+    if (typeof $.scrollTo === 'function') {
+        $('a.scroll').bind('click', function () {
+            var a = $(this),
+            scroll_to = $(a.attr('href'));
+            if (scroll_to.length > 0) {
+                $.scrollTo(scroll_to, 1000);
+            }
 
-        return false;
-    });
+            return false;
+        });
+    }
 
     if (typeof $.fancybox === 'function') {
         $('a.lightbox').fancybox({
