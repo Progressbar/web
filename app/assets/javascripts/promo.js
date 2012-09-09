ecd25a5fc10f3d7d8b1e44c487bfe59ae71ce5ae
progressbarsk <keram@trip.sk>
2012-09-09 03:59:06 +0200

    update promo js/css

--- app/assets/javascripts/promo.js
+++ app/assets/javascripts/promo.js
@@ -1,64 +1,61 @@
 $(function() {
-    var promo = $('#promoss');
+    var promo = $('#promo'),
+        promo_cnt = $('#promo-content'),
+        promo_nav = $('#promo-nav'),
+        promo_items = [], promo_active_item = null, promo_auto_animate = true, promo_timer = null;
 
     if (promo.length > 0) {
-        var promo_cnt = $('#promo-content'),
-            promo_nav = $('#promo-nav'),
-            promo_items = promo.find('.promo-item'),
-            promo_active_item = null,
-            promo_auto_animate = true,
-            promo_timer = null,
-            loc_hash = document.location.hash;
+        promo_items = promo.find('.promo-item');
 
-        if (promo.find(loc_hash).length === 0) {
-            function promoAnimate() {
-                promo_items.each(function () {
-                    var elm = $(this),
+        promo_nav.delegate('a', 'click', function () {
+            clearTimeout(promo_timer);
+            promo_auto_animate = false;
+            promo_active_item = $($(this).attr('href')).get(0);
+            promoAnimate();
+            return false;
+        });
+
+        function promoAnimate() {
+            promo_items.each(function () {
+                var elm = $(this),
                     elm_anchor = promo_nav.find('a[href|="#' + this.id + '"]');
 
-                    if (this.id != promo_active_item.id) {
-                        elm.removeClass('active');
-                        elm_anchor.removeClass('active');
-                        elm.fadeTo('normal', 0.3);
-                    } else {
-                        elm.addClass('active');
-                        elm_anchor.addClass('active');
-                        elm.fadeTo('normal', 1);
-                    }
-                });
-            }
-
-            promo_nav.delegate('a', 'mouseover', function () {
-                clearTimeout(promo_timer);
-                promo_auto_animate = false;
-                promo_active_item = $($(this).attr('href')).get(0);
-                promoAnimate();
+                if (this.id != promo_active_item.id) {
+                    elm.removeClass('active');
+                    elm_anchor.removeClass('active');
+                    elm.fadeTo('normal', 0.3);
+                } else {
+                    elm.addClass('active');
+                    elm_anchor.addClass('active');
+                    elm.fadeTo('fast', 1);
+                }
             });
+        }
 
-            function promoAnimateTimer () {
-                promo_timer = setTimeout(function () {
-                    if (promo_auto_animate && promo_active_item !== promo_items.last()) {
-                        var active_index = promo_active_item ? promo_items.index(promo_active_item) : -1;
-                        promo_active_item = promo_items.get(active_index + 1);
+        function promoAnimateTimer () {
+            var last_item = promo_items.last();
+            promo_timer = setTimeout(function () {
+                if (promo_auto_animate && promo_active_item !== last_item) {
+                    var active_index = promo_active_item ? promo_items.index(promo_active_item) : -1;
+                    promo_active_item = promo_items.get(active_index + 1);
 
-                        if (promo_active_item) {
-                            promoAnimate();
-                            promoAnimateTimer();
-                        } else {
-                            clearTimeout(promo_timer);
-                            promo_auto_animate = false;
-                        }
+                    if (promo_active_item) {
+                        promoAnimate();
+                        promoAnimateTimer();
                     } else {
                         clearTimeout(promo_timer);
                         promo_auto_animate = false;
                     }
-                }, promo_active_item ? 4000 : 10);
+                } else {
+                    clearTimeout(promo_timer);
+                    promo_auto_animate = false;
+                }
+            }, promo_active_item ? 4000 : 10);
 
-                return false;
-            }
-
-//            promoAnimateTimer();
+            return false;
         }
+
+        promoAnimateTimer();
     }
 
 });
