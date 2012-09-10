929ef7fa0dffebc78a9d9ed5032149761c46bf2d
progressbarsk <keram@trip.sk>
2012-09-10 03:02:35 +0200

    decorate instead of overwrite user model class

---
+++ app/decorators/models/refinery/user_model_decorator.rb
@@ -0,0 +1,24 @@
+Refinery::User.class_eval do
+  has_many :fees, :class_name => '::Refinery::Fees::Fee'
+
+  before_save :generate_progressbar_uid, :if => :empty_progressbar_uid
+
+  def identifications
+    trans = ::Refinery::Transactions::Transaction.income.where(:vs => self.progressbar_uid)
+    acc = []
+    trans.each do |t|
+      acc << t.from_account
+    end
+    "#{self.progressbar_uid} - #{self.username} - #{self.progressbar_screenname.to_s} - #{acc.uniq.join(' - ').to_s}"
+  end
+
+  private
+
+    def generate_progressbar_uid
+      self.progressbar_uid = ::Refinery::User.select('MAX(progressbar_uid) AS progressbar_uid').last.progressbar_uid.to_i + 42
+    end
+
+    def empty_progressbar_uid
+      self.progressbar_uid.nil? or self.progressbar_uid == 0
+    end
+end
